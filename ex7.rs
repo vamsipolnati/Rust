d9fcaea85a399663af70f4dab56c63a6e0c4d2f6
vamsipolnati <155517494+vamsipolnati@users.noreply.github.com>
2025-07-11 10:04:57 +0530

    Create ex7.rs

---
+++ ex7.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let tup = (1, "Rust", 3.14, true);
+
+    println!("First element of tuple: {}", tup.0);
+    println!("Second element of tuple: {}", tup.1);
+    println!("Third element of tuple: {}", tup.2);
+    println!("Fourth element of tuple: {}", tup.3);
+}
