77b2d11471c4e36f15450107864493385d38635c
vamsipolnati <155517494+vamsipolnati@users.noreply.github.com>
2025-07-11 09:56:24 +0530

    ex5.rs

---
+++ ex5.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x = 10;
+    let y = 3.14;
+
+    println!("Implicit Type Declaration:");
+    println!("Value of x: {}", x);
+    println!("Value of y: {}", y);
+
+    let a: i32 = 20;
+    let b: f64 = 2.718;
+    let c: bool = true;
+
+    println!("\nExplicit Type Declaration:");
+    println!("Value of a: {}", a);
+    println!("Value of b: {}", b);
+    println!("Value of c: {}", c);
+}
